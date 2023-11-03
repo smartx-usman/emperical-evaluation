66c651ed70135047115c922f1dab3549e3409994
smartx-usman <usman@smartx.kr>
2023-11-03 09:43:42 +0100

    Update snapshot IDs.

--- experiment-lightweight-kubernetes/experimentation-tools/provision-vm-instances.sh
+++ experiment-lightweight-kubernetes/experimentation-tools/provision-vm-instances.sh
@@ -10,10 +10,10 @@ microstack.openstack server create --image  ubuntu22.04 --security-group de309f1
 microstack.openstack server create --image  ubuntu22.04 --security-group de309f13-7c25-4b6b-86a1-95c5f9e3c02c --availability-zone nova:worker3 --flavor m1.small --network 1c18ccdb-e038-442c-bb77-68383fd3a359 worker3
 
 # Create VM instances from snapshots
-microstack.openstack server create --image  1ea7ab0a-a335-4d88-8a79-95dd9fffc5c2 --security-group de309f13-7c25-4b6b-86a1-95c5f9e3c02c --availability-zone nova:master --flavor m1.medium --network 1c18ccdb-e038-442c-bb77-68383fd3a359 master1
-microstack.openstack server create --image  554ed5ef-4753-455f-b300-8041f984eae4 --security-group de309f13-7c25-4b6b-86a1-95c5f9e3c02c --availability-zone nova:worker1 --flavor m1.small --network 1c18ccdb-e038-442c-bb77-68383fd3a359 worker1
-microstack.openstack server create --image  b7a6ef6b-5387-4b1c-a80e-a2c92e4629cd --security-group de309f13-7c25-4b6b-86a1-95c5f9e3c02c --availability-zone nova:worker2 --flavor m1.small --network 1c18ccdb-e038-442c-bb77-68383fd3a359 worker2
-microstack.openstack server create --image  1c36d2fc-b99a-4c80-a81b-ab36b667b3ff --security-group de309f13-7c25-4b6b-86a1-95c5f9e3c02c --availability-zone nova:worker3 --flavor m1.small --network 1c18ccdb-e038-442c-bb77-68383fd3a359 worker3
+microstack.openstack server create --image  7e49bed9-d779-47f8-a600-9f28488a2110 --security-group de309f13-7c25-4b6b-86a1-95c5f9e3c02c --availability-zone nova:master --flavor m1.medium --network 1c18ccdb-e038-442c-bb77-68383fd3a359 master1
+microstack.openstack server create --image  20d876fc-d18a-4a47-881a-dcba651022c7 --security-group de309f13-7c25-4b6b-86a1-95c5f9e3c02c --availability-zone nova:worker1 --flavor m1.small --network 1c18ccdb-e038-442c-bb77-68383fd3a359 worker1
+microstack.openstack server create --image  1094c961-5a58-40a8-add5-f7662b53e59c --security-group de309f13-7c25-4b6b-86a1-95c5f9e3c02c --availability-zone nova:worker2 --flavor m1.small --network 1c18ccdb-e038-442c-bb77-68383fd3a359 worker2
+microstack.openstack server create --image  1375c995-ee36-4de2-b83a-af930446c4b5 --security-group de309f13-7c25-4b6b-86a1-95c5f9e3c02c --availability-zone nova:worker3 --flavor m1.small --network 1c18ccdb-e038-442c-bb77-68383fd3a359 worker3
 
 #microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker3 --flavor m1.small --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker8
 #microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker3 --flavor m2.tiny --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker9
