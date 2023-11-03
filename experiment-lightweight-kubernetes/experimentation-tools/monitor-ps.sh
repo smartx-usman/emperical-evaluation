4bd9c0b86f006d20e809ee96f9d2ec55d9219824
smartx-usman <usman@smartx.kr>
2023-11-03 09:17:33 +0100

    Add variable hostname.

--- experiment-lightweight-kubernetes/experimentation-tools/monitor-ps.sh
+++ experiment-lightweight-kubernetes/experimentation-tools/monitor-ps.sh
@@ -2,8 +2,9 @@
 
 # Check for the number of command-line arguments
 if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <process_name> <distribution> <iterations> <wait_time>"
-    echo "E.g., ./monitor.sh microk8s mater 10 5"
+    echo "Usage: $0 <distribution> <node_type> <iterations> <wait_time>"
+    echo "E.g., ./monitor-ps.sh microk8s master 10 5"
+    echo "E.g., ./monitor-ps.sh k0s worker 50 5"
     exit 1
 fi
 
@@ -21,42 +22,40 @@ iterations=$3
 # Wait time between each run (in seconds)
 wait_time=$4
 
+# Get the hostname for naming output files
+host=$(hostname)
+
 # Check if the output file already exists, then remove them
 find "$HOME_PATH" -type f -name "pu_*" -exec rm {} \;
 
-if [ "$node" = "worker1" ]; then
+if [ "$node" = "worker" ]; then
   if [ "$distribution" = "microk8s" ]; then
     for ((i = 1; i <= iterations; i++)); do
-      pidstat -r -h -C "kubelite|containerd|calico-node" | grep -E -v "containerd-shim" >> pu_memory_worker1.txt &
-      pidstat -u -h -C "kubelite|containerd|calico-node" | grep -E -v "containerd-shim" >> pu_cpu_worker1.txt &
+      pidstat -r -h -C "kubelite|containerd|calico-node" | grep -E -v "containerd-shim" >> "pu_memory_${host}.txt" &
+      pidstat -u -h -C "kubelite|containerd|calico-node" | grep -E -v "containerd-shim" >> "pu_cpu_${host}.txt" &
       sleep $wait_time
     done
   else
     for ((i = 1; i <= iterations; i++)); do
-      pidstat -r -h -C "kubelet|containerd" | grep -E -v "containerd-shim" >> pu_memory_worker1.txt &
-      pidstat -u -h -C "kubelet|containerd" | grep -E -v "containerd-shim" >> pu_cpu_worker1.txt &
+      pidstat -r -h -C "kubelet|containerd" | grep -E -v "containerd-shim" >> "pu_memory_${host}.txt" &
+      pidstat -u -h -C "kubelet|containerd" | grep -E -v "containerd-shim" >> "pu_cpu_${host}.txt" &
       sleep $wait_time
     done
   fi
 elif [ "$node" = "master" ]; then
   if [ "$distribution" = "microk8s" ]; then
     for ((i = 1; i <= iterations; i++)); do
-      pidstat -r -h -C "k8s-dqlite|kubelite|calico-node" >> pu_memory_master.txt &
-      pidstat -u -h -C "k8s-dqlite|kubelite|calico-node" >> pu_cpu_master.txt &
+      pidstat -r -h -C "k8s-dqlite|kubelite|calico-node" >> "pu_memory_${host}.txt" &
+      pidstat -u -h -C "k8s-dqlite|kubelite|calico-node" >> "pu_cpu_${host}.txt" &
       sleep $wait_time
     done
   else
     for ((i = 1; i <= iterations; i++)); do
-      pidstat -r -h -C "etcd|kube-apiserver" >> pu_memory_master.txt &
-      pidstat -u -h -C "etcd|kube-apiserver" >> pu_cpu_master.txt &
+      pidstat -r -h -C "etcd|kube-apiserver" >> "pu_memory_${host}.txt" &
+      pidstat -u -h -C "etcd|kube-apiserver" >> "pu_cpu_${host}.txt" &
       sleep $wait_time
     done
   fi
-else
-  echo "Invalid argument. Exiting for now."
-  echo "Please enter the command in format: ./monitor.sh distribution node iterations wait"
-  echo "E.g., ./monitor-ps.sh k0s master 10 5"
-  exit 1
 fi
 
 echo "All the commands have completed. check output files."
