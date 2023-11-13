e4565242c761a6c906968d4ceb2fe498f53f842e
smartx-usman <usman@smartx.kr>
2023-11-13 16:10:12 +0100

    Simplify script for dp experiments.

--- experiment-lightweight-kubernetes/experimentation-tools/dp-experiment.sh
+++ experiment-lightweight-kubernetes/experimentation-tools/dp-experiment.sh
@@ -1,16 +1,74 @@
 #!/bin/bash
 
-# include configurator script
-source ./configurator.sh
+Red='\033[0;31m'
+Green='\e[32m'
+Yellow='\033[0;33m'
+BBlue='\e[44m'
 
+LOGFILE="./dp.log"
+HOME_PATH="./data"
+
+# Default values
 result_file="result.txt"
 coap_server="single"
-coap_port="31839" # or 5683
+coap_port="5683" # or 31839
 
-rm -rf $result_file
+handle_debug() {
+  { set +x; } 2>/dev/null; echo -n "[$(date -Is)]  "; set -x
+}
 
+handle_error() {
+  echo -e "\e[31mERROR: An error occurred during execution, check log $LOGFILE for details.\e[0m"
+  exit 1
+}
+
+exec 3>&1 1>"$LOGFILE" 2>&1
+
+set -ex # Prints commands, prefixing them with a character stored in an environmental variable ($PS4)
+trap "handle_error" ERR
+trap '{ set +x; } 2>/dev/null; echo -n "[$(date -Is)]  "; set -x' DEBUG
+
+# Function to display usage information
+usage() {
+  echo "Usage: $0 [-f <result_file>] [-s <coap_server>] [-p <coap_port>]" >&3
+  echo "  -f <result_file>: Specify the result file (default: result.txt)" >&3
+  echo "  -s <coap_server>: Specify the CoAP server (default: single)" >&3
+  echo "  -p <coap_port>: Specify the CoAP port (default: 5683)" >&3
+  exit 1
+}
+
+# Parse command line arguments
+while getopts ":f:s:p:" opt; do
+  case $opt in
+    f)
+      result_file="$OPTARG"
+      ;;
+    s)
+      coap_server="$OPTARG"
+      ;;
+    p)
+      coap_port="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument."
+      usage
+      ;;
+  esac
+done
+
+# Check if the file exists
+if [ -e "$result_file" ]; then
+  rm "$result_file"
+fi
+
+# Install CoAPthon3 library
 pip3 install CoAPthon3
 
+# Run experiments
 for experiment in 1 2 3 4 5
 do
   echo "[........... Experiment No. $experiment ..........]" >> $result_file
