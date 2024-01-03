05b82402593521c1b312b24bfbb5a0abb9392294
smartx-usman <usman@smartx.kr>
2024-01-03 13:49:45 +0100

    Add Microshift.

--- lightweight-container-orchestrator/plot-pu.py
+++ lightweight-container-orchestrator/plot-pu.py
@@ -8,7 +8,7 @@ pd.set_option('display.max_rows', None)
 pd.set_option('mode.chained_assignment', None)
 
 # Subplots layout
-no_of_rows = 3
+no_of_rows = 4
 no_of_cols = 2
 sharex = 'col'
 sharey = 'none'
@@ -23,11 +23,13 @@ my_plot_object = Plots(no_of_rows, no_of_cols, sharex, sharey, legend_columns, f
 workers = 1  # 1, 2, 3, 4, 5
 run = 1
 deployment = 'pod'  # 'pod' or 'deployment'
-node = 'worker1'  # 'master1' or 'worker1'
+node = 'master1'  # 'master1' or 'worker1'
 color = 'tab20'
 
-files_path = [f'microk8s/pu_{workers}worker_{run}run', f'k0s/pu_{workers}worker_{run}run',
-              f'k3s/pu_{workers}worker_{run}run', f'openshift/pu_{workers}worker_{run}run']
+files_path = [f'k0s/pu_{workers}worker_{run}run',
+              f'k3s/pu_{workers}worker_{run}run',
+              f'microk8s/pu_{workers}worker_{run}run',
+              f'microshift/pu_{workers}worker_{run}run']
 
 
 def cpu_usage(input_file, axs_row, axs_col, title, x_label, y_label, y_lim_start, y_lim_end, legend_set, set_x_label,
@@ -101,7 +103,7 @@ def memory_usage(input_file, axs_row, axs_col, title, x_label, y_label, y_lim_st
     stacked_df = concatenated_df.pivot_table(index='time_seconds', columns='command', values='value',
                                              aggfunc='sum').fillna(0).cumsum(axis=1)
 
-    print(stacked_df.head(4))
+    #print(stacked_df.head(4))
     my_plot_object.time_instance_stack_plot(df=stacked_df, axs_row=axs_row, axs_col=axs_col,
                                             title=title, x_label=x_label, y_label=y_label,
                                             y_lim_start=y_lim_start, y_lim_end=y_lim_end, legend_set=legend_set,
@@ -112,12 +114,13 @@ def call_plot(path, distribution, color, services, function, axs_row, axs_col, l
     if function == 'cpu':
         cpu_usage(input_file=f'{path}/pu_cpu_{node}.csv', axs_row=axs_row, axs_col=axs_col,
                   title=f'{distribution} Services (CPU)',
-                  x_label="Time (sec)", y_label="Usage (%)", y_lim_start=0, y_lim_end=10,
+                  x_label="Time (sec)", y_label="Usage (%)", y_lim_start=0, y_lim_end=60,
                   legend_set=legend, set_x_label=True, label=distribution, color=color, services=services)
+
     if function == 'memory':
         memory_usage(input_file=f'{path}/pu_memory_{node}.csv', axs_row=axs_row, axs_col=axs_col,
                      title=f'{distribution} Services (Memory)',
-                     x_label="Time (sec)", y_label="Usage (MBytes)", y_lim_start=0, y_lim_end=2048,
+                     x_label="Time (sec)", y_label="Usage (MBytes)", y_lim_start=0, y_lim_end=4096,
                      legend_set=legend, set_x_label=True, label=distribution, color=color, services=services)
 
 
@@ -126,32 +129,35 @@ def process_data():
     microk8s_services = ['K8s-dqlite', 'Kubelite', 'Containerd', 'calico-node']
     k0s_services = ['Etcd', 'Kube-apiserver', 'K0s', 'Kubelet', 'Containerd', 'calico-node']
     k3s_services = ['k3s-server', 'k3s-agent', 'Containerd', 'flannel']
-    openshift_services = ['Etcd', 'Kube-apiserver', 'Kubelet', 'Containerd']
+    microshift_services = ['Microshift', 'Microshift-etcd', 'CRI-O', 'OVN-controller', 'OVNkube']
 
-    for distribution in ('Microk8s', 'K0s', 'K3s'):
-        if distribution == 'Microk8s':
+    for distribution in ('K0s', 'K3s', 'Microk8s', 'Microshift'):
+        if distribution == 'K0s':
             path = files_path[0]
-            call_plot(path=path, distribution=distribution, color=color, services=microk8s_services, function='cpu',
+            call_plot(path=path, distribution=distribution, color=color, services=k0s_services, function='cpu',
                       axs_row=0, axs_col=0, legend=False)
-            call_plot(path=path, distribution=distribution, color=color, services=microk8s_services, function='memory',
+            call_plot(path=path, distribution=distribution, color=color, services=k0s_services, function='memory',
                       axs_row=0, axs_col=1, legend=True)
 
-        elif distribution == 'K0s':
-            path = files_path[1]
-            call_plot(path=path, distribution=distribution, color=color, services=k0s_services, function='cpu',
-                      axs_row=1, axs_col=0, legend=False)
-            call_plot(path=path, distribution=distribution, color=color, services=k0s_services, function='memory',
-                      axs_row=1, axs_col=1, legend=True)
-
         elif distribution == 'K3s':
-            path = files_path[2]
+            path = files_path[1]
             call_plot(path=path, distribution=distribution, color=color, services=k3s_services, function='cpu',
-                      axs_row=2, axs_col=0, legend=False)
+                      axs_row=1, axs_col=0, legend=False)
             call_plot(path=path, distribution=distribution, color=color, services=k3s_services, function='memory',
+                      axs_row=1, axs_col=1, legend=True)
+        elif distribution == 'Microk8s':
+            path = files_path[2]
+            call_plot(path=path, distribution=distribution, color=color, services=microk8s_services, function='cpu',
+                      axs_row=2, axs_col=0, legend=False)
+            call_plot(path=path, distribution=distribution, color=color, services=microk8s_services, function='memory',
                       axs_row=2, axs_col=1, legend=True)
 
-        elif distribution == 'OpenShift':
+        elif distribution == 'Microshift':
             path = files_path[3]
+            call_plot(path=path, distribution=distribution, color=color, services=microshift_services, function='cpu',
+                      axs_row=3, axs_col=0, legend=False)
+            call_plot(path=path, distribution=distribution, color=color, services=microshift_services, function='memory',
+                      axs_row=3, axs_col=1, legend=True)
 
         else:
             continue
