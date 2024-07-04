4b34d99d8a27c5fec5e74d32686512d15f28893a
smartx-usman <usman@smartx.kr>
2024-07-04 13:10:06 +0200

    Updated plotting scripts.

--- lightweight-container-orchestrator/plot-pu.py
+++ lightweight-container-orchestrator/plot-pu.py
@@ -7,26 +7,37 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('mode.chained_assignment', None)
 
-# Subplots layout
-no_of_rows = 3
-no_of_cols = 2
-sharex = 'col'
-sharey = 'none'
-legend_columns = 1
-figure_width = 7.0  # for 4 rows
-figure_width = 5.0  # for 3 rows
-figure_height = 8.0 # for 4 rows
-figure_height = 6.0  # for 3 rows
-
-# Create the plot object
-my_plot_object = Plots(no_of_rows, no_of_cols, sharex, sharey, legend_columns, figure_width, figure_height)
-
 # Define experiment variables
 workers = 1  # 1, 2, 3, 4, 5
 run = 1
 deployment = 'pod'  # 'pod' or 'deployment'
 node = 'worker1'  # 'master1' or 'worker1'
 color = 'Accent'
+
+# Subplots layout
+if node == 'master1':
+    no_of_rows = 2
+    no_of_cols = 4
+    figure_width = 8.0
+    figure_height = 5.0
+else:
+    no_of_rows = 2
+    no_of_cols = 3
+    figure_width = 8.0
+    figure_height = 5.0
+
+sharex = 'col'
+sharey = 'row'
+legend_columns = 1
+#figure_width = 7.0  # for 4 rows
+#figure_width = 5.0  # for 3 rows
+#figure_height = 8.0 # for 4 rows
+#figure_height = 6.0  # for 3 rows
+
+# Create the plot object
+my_plot_object = Plots(no_of_rows, no_of_cols, sharex, sharey, legend_columns, figure_width, figure_height)
+
+
 
 files_path = [f'k0s/pu_{workers}worker_{run}run',
               f'k3s/pu_{workers}worker_{run}run',
@@ -35,7 +46,7 @@ files_path = [f'k0s/pu_{workers}worker_{run}run',
 
 
 def cpu_usage(input_file, axs_row, axs_col, title, x_label, y_label, y_lim_start, y_lim_end, legend_set, set_x_label,
-              label, color, services):
+              set_y_label, label, color, services):
     """Plot CPU Usage"""
     df = pd.read_csv(input_file)
     df['time'] = pd.to_datetime(df['timestamp'])
@@ -73,11 +84,12 @@ def cpu_usage(input_file, axs_row, axs_col, title, x_label, y_label, y_lim_start
                                             title=title, x_label=x_label, y_label=y_label,
                                             y_lim_start=y_lim_start, y_lim_end=y_lim_end,
                                             legend_set=legend_set, legend_outside=False,
-                                            set_x_label=set_x_label, color=color, services=services)
+                                            set_x_label=set_x_label, set_y_label=set_y_label,
+                                            color=color, services=services)
 
 
 def memory_usage(input_file, axs_row, axs_col, title, x_label, y_label, y_lim_start, y_lim_end, legend_set, set_x_label,
-                 label, color, services):
+                 set_y_label, label, color, services):
     """Plot CPU Usage"""
     df = pd.read_csv(input_file)
     df['time'] = pd.to_datetime(df['timestamp'])
@@ -112,19 +124,20 @@ def memory_usage(input_file, axs_row, axs_col, title, x_label, y_label, y_lim_st
                                             title=title, x_label=x_label, y_label=y_label,
                                             y_lim_start=y_lim_start, y_lim_end=y_lim_end,
                                             legend_set=legend_set, legend_outside=False,
-                                            set_x_label=set_x_label, color=color, services=services)
+                                            set_x_label=set_x_label, set_y_label=set_y_label,
+                                            color=color, services=services)
 
 
-def call_plot(path, distribution, color, services, function, axs_row, axs_col, legend):
+def call_plot(path, distribution, color, services, function, axs_row, axs_col, legend, set_y_label=False):
     if function == 'cpu':
         if node == 'master1':
-            y_lim_end = 60
+            y_lim_end = 100
         else:
-            y_lim_end = 30
+            y_lim_end = 100
         cpu_usage(input_file=f'{path}/pu_cpu_{node}.csv', axs_row=axs_row, axs_col=axs_col,
-                  title=f'{distribution} - CPU',
-                  x_label="Time (sec)", y_label="Usage (%)", y_lim_start=0, y_lim_end=y_lim_end,
-                  legend_set=legend, set_x_label=True, label=distribution, color=color, services=services)
+                  title=f'{distribution}',
+                  x_label="Time (sec)", y_label="CPU Usage (%)", y_lim_start=0, y_lim_end=y_lim_end,
+                  legend_set=legend, set_x_label=True, set_y_label=set_y_label, label=distribution, color=color, services=services)
 
     if function == 'memory':
         if node == 'master1':
@@ -132,9 +145,9 @@ def call_plot(path, distribution, color, services, function, axs_row, axs_col, l
         else:
             y_lim_end = 2048
         memory_usage(input_file=f'{path}/pu_memory_{node}.csv', axs_row=axs_row, axs_col=axs_col,
-                     title=f'{distribution} - Memory',
-                     x_label="Time (sec)", y_label="Usage (MBytes)", y_lim_start=0, y_lim_end=y_lim_end,
-                     legend_set=legend, set_x_label=True, label=distribution, color=color, services=services)
+                     title=f'',
+                     x_label="Time (sec)", y_label="Memory Usage (MB)", y_lim_start=0, y_lim_end=y_lim_end,
+                     legend_set=legend, set_x_label=True, set_y_label=set_y_label, label=distribution, color=color, services=services)
 
 
 def process_data():
@@ -162,33 +175,34 @@ def process_data():
         if distribution == 'K0s':
             path = files_path[0]
             call_plot(path=path, distribution=distribution, color=color, services=k0s_services, function='cpu',
-                      axs_row=0, axs_col=0, legend=True)
+                      axs_row=0, axs_col=0, set_y_label=True, legend=True)
             call_plot(path=path, distribution=distribution, color=color, services=k0s_services, function='memory',
-                      axs_row=0, axs_col=1, legend=False)
+                      axs_row=1, axs_col=0, set_y_label=True, legend=False)
         elif distribution == 'K3s':
             path = files_path[1]
             call_plot(path=path, distribution=distribution, color=color, services=k3s_services, function='cpu',
-                      axs_row=1, axs_col=0, legend=True)
+                      axs_row=0, axs_col=1, legend=True)
             call_plot(path=path, distribution=distribution, color=color, services=k3s_services, function='memory',
                       axs_row=1, axs_col=1, legend=False)
         elif distribution == 'Microk8s':
             path = files_path[2]
             call_plot(path=path, distribution=distribution, color=color, services=microk8s_services, function='cpu',
-                      axs_row=2, axs_col=0, legend=True)
+                      axs_row=0, axs_col=2, legend=True)
             call_plot(path=path, distribution=distribution, color=color, services=microk8s_services, function='memory',
-                      axs_row=2, axs_col=1, legend=False)
+                      axs_row=1, axs_col=2, legend=False)
 
         elif distribution == 'Microshift':
             path = files_path[3]
             call_plot(path=path, distribution=distribution, color=color, services=microshift_services, function='cpu',
-                      axs_row=3, axs_col=0, legend=True)
+                      axs_row=0, axs_col=3, legend=True)
             call_plot(path=path, distribution=distribution, color=color, services=microshift_services,
                       function='memory',
-                      axs_row=3, axs_col=1, legend=False)
+                      axs_row=1, axs_col=3, legend=False)
 
         else:
             continue
 
 
 process_data()
+#plt.show()
 plt.savefig(f'figures/pu_{deployment}_{workers}worker_{node}.png', dpi=600)
